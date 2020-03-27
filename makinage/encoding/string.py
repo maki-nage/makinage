3a440dde7b6df90cb0661d721fb3fa849a6ea3b8
Romain Picard <romain.picard@oakbits.com>
2020-03-27 23:31:59 +0100

    encoding: add doc to string encoder

--- makinage/encoding/string.py
+++ makinage/encoding/string.py
@@ -1,6 +1,13 @@
 
 
 def encoder(encoding="utf-8"):
+    ''' String encoder
+
+    encodes strings using the encode/decode python functions
+
+    Args:
+        encoding: The string encoding to use.
+    '''
     def _encode(i):
         return i.encode(encoding)
 
