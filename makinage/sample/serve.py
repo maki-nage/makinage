719ed7646124afe2c0d18091b8512d723012bcf9
Romain Picard <romain.picard@oakbits.com>
2020-07-17 11:17:34 +0200

    serve: fix unit tests

--- makinage/sample/serve.py
+++ makinage/sample/serve.py
@@ -1,4 +1,19 @@
 
 
-def prepare_function(i):
-    return i
+def pre_transform(config):
+    field = config['config']['serve']['input_field']
+
+    def _transform(i):
+        return i[field]
+
+    return _transform
+
+
+def post_transform(config):
+    field = config['config']['serve']['output_field']
+
+    def _transform(i, pred):
+        i[field] = pred
+        return i
+
+    return _transform
--- tests/test_serve.py
+++ tests/test_serve.py
@@ -1,17 +1,10 @@
 from makinage.serve.serve import create_transform_functions, infer
-import makinage.sample.serve as sample_serve
 
 import numpy as np
 
 
-def test_create_pre_transform_present():
-    c = { 'config': { 'serve': { 'pre_transform': 'makinage.sample.serve:prepare_function'}}}
-    t = create_transform_functions(c)
-    assert t.pre == sample_serve.prepare_function
-
-
 def test_create_transform_default():
-    c = { 'config': { 'serve': {}}}
+    c = {'config': {'serve': {}}}
     t = create_transform_functions(c)
     assert t.pre == np.array
     assert t.post == list
@@ -25,12 +18,14 @@ def test_predict():
         predict_count += 1
         return np.array([i+1])
 
-    c = { 'config': { 'serve': { 'input_field': 'x', 'output_field': 'pred'}}}
+    c = {'config': {'serve': {
+        'pre_transform': 'makinage.sample.serve:pre_transform',
+        'post_transform': 'makinage.sample.serve:post_transform',
+        'input_field': 'x', 'output_field': 'pred',
+    }}}
     t = create_transform_functions(c)
-    data = { 'x': 42 }
-    actual_result = infer(data, c, t, _predict)
+    data = {'x': 42}
+    actual_result = infer(data, t, _predict)
 
     assert predict_count == 1
-    assert actual_result == { 'x': 42, 'pred': [43] }
-
-    
+    assert actual_result == {'x': 42, 'pred': [43]}
