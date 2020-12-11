2f1499d66b1186d557ee3401fb2a6719d97578ad
Romain Picard <romain.picard@oakbits.com>
2020-12-11 17:15:56 +0100

    fix sample processor code

--- makinage/sample/processor.py
+++ makinage/sample/processor.py
@@ -1,12 +1,12 @@
 import rx.operators as ops
-import rxsci
+import rxsci as rs
 from cyclotron.debug import trace_observable
 
 
 def multiply(config, values):
     sink = values.pipe(
         trace_observable("multiply 1"),
-        rxsci.with_latest_from(config),
+        rs.ops.with_latest_from(config),
         ops.starmap(lambda i, c: int(i) * c['config']['multiply']),
         trace_observable("multiplied"),
         ops.map(lambda i: str(i))
